a100ebec08593a5e4f7cb0bd826a3c36bb47ae87
RuslanBespansky <ruslan.bespansky@yandex.ru>
2024-02-13 11:08:24 +0300

    25 lesson

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -11,35 +11,39 @@ const DialogItem = (props: any) => {
   );
 };
 
-const Message = (props:any)  => {
-  return (
-    <div className={classes.message}>{props.message}</div>
-
-  )
-}
-
-
+const Message = (props: any) => {
+  return <div className={classes.message}>{props.message}</div>;
+};
 
 export const Dialogs = () => {
+  let dialogsData = [
+    { id: 1, name: "Ruslan" },
+    { id: 2, name: "Max" },
+    { id: 3, name: "Inna" },
+    { id: 4, name: "Artur" },
+    { id: 5, name: "Lena" },
+    { id: 6, name: "Veronika" },
+  ];
+
+  let messages = [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "How are you?" },
+    { id: 3, message: "Hello" },
+  ];
+
+  let dialogsElements = dialogsData.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+
+  let messageElements = messages.map((m) => (
+    <Message message={m.message} id={m.id} />
+  ));
+
   return (
     <div className={classes.dialogs}>
-      <div className={classes.dialogs_item}>
-        <DialogItem name="Ruslan" id="1" />
-
-        <DialogItem name="Max" id="2" />
-
-        <DialogItem name="Inna" id="3" />
-
-        <DialogItem name="Artur" id="4" />
-
-        <DialogItem name="Katya" id="5" />
-
-        <DialogItem name="Veronika" id="6" />
-      </div>
+      <div className={classes.dialogs_item}>{dialogsElements}</div>
       <div className={classes.messages}>
-       <Message message = "Hi"/>
-       <Message message = "How are you?"/>
-       <Message message = "Hello"/>
+        {messageElements}
        
       </div>
     </div>
--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import classes from "./Myposts_css.module.css";
 import { Post } from "./post/Post";
 export const MyPosts = () => {
+  let postData = [
+    { id: 1, message: "Hi, how are you?", like: 15 },
+    { id: 2, message: "It's  my firs post", like: 8 },
+    { id: 3, message: "It's  me", like: 23 },
+    { id: 4, message: "HELLO", like: 5 },
+  ];
+
+let postsElements = postData.map(p =>  <Post message={p.message} like={p.like} />);
+
+
   return (
     <div className={classes.postsBlock}>
-  
       {" "}
-     <h3>
-     my post
-      </h3>
+      <h3>my post</h3>
       <div>
         <div>
           <textarea></textarea>
@@ -18,8 +25,9 @@ export const MyPosts = () => {
         </div>
       </div>
       <div className={classes.posts}>
-        <Post message={"Hi, how are you?"} like={15} />
-        <Post message="It's  my firs post" like={9} />
+
+        {postsElements}
+      
       </div>
     </div>
   );
